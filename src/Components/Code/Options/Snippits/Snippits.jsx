2b5e32f3215f5305f934cc5c5fcc643313d5526c
megabeing <jain.sahil58158@gmail.com>
2025-03-30 14:38:40 +0530

    Enhance Snippits component with search functionality and improve styling for better user experience

--- src/Components/Code/Options/Snippits/Snippits.jsx
+++ src/Components/Code/Options/Snippits/Snippits.jsx
@@ -4,14 +4,26 @@ import styles from "./Snippits.module.less"
 import { snippitData } from "./data"
 export default function Snippits({ addElement }) {
     const [filteredSnippits, setFilteredSnippits] = useState(snippitData);
+    const filterSnippit = (search) => {
+        if (search) {
+            const filtered = filteredSnippits.filter((ele) => {
+                const low = ele.title.toLowerCase()
+                return low.includes(search.toLowerCase())
+            })
+            setFilteredSnippits(filtered)
+        }
+        else {
+            setFilteredSnippits(snippitData)
+        }
+    }
     return (
         <div className="">
             <div className="text-center text-sm mb-4">
                 Click on a section below to add
             </div>
             <div className={styles.container}>
-                <input placeholder="Search" className="rounded-full px-5 py-3 drop-shadow-xl w-full"></input>
-                <div className="mt-5 w-full max-h-40 overflow-y-auto pr-3">
+                <input placeholder="Search" className={styles.search} onChange={(e) => filterSnippit(e.target.value)} />
+                <div className={styles.snippitListContainer}>
                     {filteredSnippits.map((ele, idx) => (<SingleSnippits {...ele} key={ele.key} addElement={addElement} />))}
                 </div>
             </div>
